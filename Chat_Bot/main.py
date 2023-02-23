597b12d2c611b4f58ce425de27f7abe10b974ab2
Tony Gama <97718437+TonyRio@users.noreply.github.com>
2023-02-23 12:58:24 -0300

    Update main.py

--- Chat_Bot/main.py
+++ Chat_Bot/main.py
@@ -1,9 +1,10 @@
 from chatterbot.trainers import ListTrainer
 from chatterbot import ChatBot
+
 bot = ChatBot('TW Chat Bot')
 
-conversa = ['Oi', 'Olá', 'Tudo bem?', 'Tudo ótimo', 
-			'Você gosta de programar?', 'Sim, eu programo em Python']
+conversa = ['Oi', 'Olá', 'Tudo bem?', 'Tudo ótimo',
+            'Você gosta de programar?', 'Sim, eu programo em Python']
 
 bot.set_trainer(ListTrainer)
 bot.train(conversa)
