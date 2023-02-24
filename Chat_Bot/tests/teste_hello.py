ccadd58734b407885374269e99ef5b2fe72689e3
Tony Gama <97718437+TonyRio@users.noreply.github.com>
2023-02-23 22:09:32 -0300

    templates

--- Chat_Bot/tests/teste_hello.py
+++
@@ -1,8 +0,0 @@
-from hello import app
-from flask import Flask
-def teste_hello():
-    response = app.test_client().get("/hello")
-
-    assert response.status_code == 200
-    assert response.data == b"Hi There"
-
